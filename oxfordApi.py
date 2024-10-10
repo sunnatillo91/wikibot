02d073775130b8b3629dc5ee5a12b388e5ebb819
Sunnatillo <sunnatillo19912006@gmail.com>
2024-10-10 22:19:11 +0500

    updates

--- oxfordApi.py
+++ oxfordApi.py
@@ -2,14 +2,14 @@ import requests
 import json
 from pprint import pprint as print
 
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
+# import sys
+# sys.stdout.reconfigure(encoding='utf-8')
 
 app_id = "9949aa54"
 app_key = "526f6635fa6ecc4b9306f52508e0efe2"
 language = "en-gb"
 
-word_id = "angel"
+word_id = "anaconda"
 
 url = "https://od-api-sandbox.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
 # url = f"https://od-api.oxforddictionaries.com:443/api/v2/entries/{language}/{word_id.lower()}"
